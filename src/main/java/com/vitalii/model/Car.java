4fdcd140a9eba0d27fd023014c2bffa141d44d21
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-03 15:13:57 +0300

    add Car class

---
+++ src/main/java/com/vitalii/model/Car.java
@@ -0,0 +1,32 @@
+package com.vitalii.model;
+
+public class Car {
+
+    private String name;
+    private int speed;
+    private int maxSpeed;
+    private int acceleration;
+
+    public Car(String name, int speed, int maxSpeed, int acceleration) {
+        this.name = name;
+        this.speed = speed;
+        this.maxSpeed = maxSpeed;
+        this.acceleration = acceleration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public int getAcceleration() {
+        return acceleration;
+    }
+}

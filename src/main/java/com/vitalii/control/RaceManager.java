7fa9e060dd6d44ff93674b3739c31b239373bb87
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-06 16:06:42 +0300

    realize adding car`s sprites to Group in RaceView by RaceManager

--- src/main/java/com/vitalii/control/RaceManager.java
+++ src/main/java/com/vitalii/control/RaceManager.java
@@ -2,15 +2,26 @@ package com.vitalii.control;
 
 import com.vitalii.model.Car;
 import com.vitalii.model.CarImpl;
+import javafx.scene.Group;
 
 import java.util.ArrayList;
 
 public class RaceManager {
 
+    private Group root;
+    private final int CARS_TO_RACE = 10;
     private ArrayList<Car> carsToRace = new CarImpl(10).getCars();
 
-    public RaceManager() {
+    public RaceManager(Group root) {
+        this.root = root;
+        int step = 20;
 
+        for (int i = 0; i < CARS_TO_RACE; i++) {
+            carsToRace.get(i).getCarView().getImageView().setX(0);
+            carsToRace.get(i).getCarView().getImageView().setY(step);
+            root.getChildren().add(carsToRace.get(i).getCarView().getImageView());
 
+            step += 50;
+        }
     }
 }

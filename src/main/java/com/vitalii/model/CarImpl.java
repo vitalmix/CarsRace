4eb91d0fe7f0ed80275e22637e24b213261decdd
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-06 17:40:42 +0300

    refactor

--- src/main/java/com/vitalii/model/CarImpl.java
+++ src/main/java/com/vitalii/model/CarImpl.java
@@ -1,15 +1,30 @@
 package com.vitalii.model;
 
+import javafx.scene.Group;
+
 import java.util.ArrayList;
 
 public class CarImpl {
 
     private ArrayList<Car> cars = new ArrayList<Car>();
 
-    public CarImpl(int amountOfCarsToImpl) {
+    public CarImpl(int amountOfCarsToImpl, Group root) {
+
+        int step = 20;
+
         for (int i = 0; i < amountOfCarsToImpl; i++) {
-            cars.add(new Car("Car " + (i + 1), (i + 1), getRandNumber(70, 100), getRandNumber(4, 7),
-                    new CarView()));
+
+            Car buffCar = new Car("Car " + (i + 1), (i + 1), getRandNumber(70, 100), getRandNumber(1, 2),
+                    new CarView());
+
+            buffCar.getCarView().getImageView().setX(0);
+            buffCar.getCarView().getImageView().setY(step);
+            root.getChildren().add(buffCar.getCarView().getImageView());
+
+            step += 50;
+
+            cars.add(buffCar);
+
         }
     }
 

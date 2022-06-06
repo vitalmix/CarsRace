5c026e8ae097687322d775c635c2d98886d08912
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-06 18:20:14 +0300

    upgrade the race logic

--- src/main/java/com/vitalii/control/RaceManager.java
+++ src/main/java/com/vitalii/control/RaceManager.java
@@ -5,53 +5,65 @@ import com.vitalii.model.CarImpl;
 import javafx.scene.Group;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RaceManager {
 
     private Group root;
-    private final int CARS_TO_RACE = 10;
-    private final int DISTANCE = 2500;
-    private ArrayList<Car> carsToRace = new CarImpl(10).getCars();
+    private final int CARS_TO_RACE = 4;
+    private final int DISTANCE = 600;
+    private ArrayList<Car> carsToRace;
+    private String winner;
 
     private boolean isRace = false;
 
     public RaceManager(Group root) {
         this.root = root;
-        int step = 20;
 
-        for (int i = 0; i < CARS_TO_RACE; i++) {
-            carsToRace.get(i).getCarView().getImageView().setX(0);
-            carsToRace.get(i).getCarView().getImageView().setY(step);
-            root.getChildren().add(carsToRace.get(i).getCarView().getImageView());
-
-            step += 50;
-        }
+        carsToRace = new CarImpl(CARS_TO_RACE, root).getCars();
     }
 
     public void startRace() {
 
+        final Object monitor = new Object();
         isRace = true;
 
         System.out.println("The race has started!");
 
-        new Thread(new Runnable() {
-            public void run() {
-                while (isRace) {
-                    for (Car car : carsToRace) {
+        final ExecutorService executorService = Executors.newFixedThreadPool(CARS_TO_RACE);
+
+        for (int i = 0; i < CARS_TO_RACE; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                public void run() {
+                    Car car = carsToRace.get(index);
+                    while (isRace) {
+                        synchronized (monitor) {
+                            if (car.getPassedDistance() >= DISTANCE) {
+                                if (winner == null) {
+                                    winner = car.getName();
+                                    isRace = false;
+                                    System.out.println(winner + " is the Winner!");
+                                    System.out.println("Passed distance: " + car.getPassedDistance());
+                                    executorService.shutdownNow();
+                                }
+                                System.out.println(car.getName() + " has passed: " + car.getPassedDistance());
+                                break;
+                            }
+                        }
                         car.move();
                         try {
-                            Thread.sleep(20);
+                            Thread.sleep(50);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        if (car.getPassedDistance() >= DISTANCE) {
-                            isRace = false;
-                            System.out.println(car.getName() + " is the Winner!");
+                            System.out.println(car.getName() + " has passed: " + car.getPassedDistance());
                             break;
                         }
                     }
                 }
-            }
-        }).start();
+            });
+        }
+
+        executorService.shutdown();
     }
 }

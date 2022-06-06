3064f8dde6500203ac491b0c342d1ad25913b38a
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-06 16:27:05 +0300

    add RaceView realization

--- src/main/java/com/vitalii/view/RaceView.java
+++ src/main/java/com/vitalii/view/RaceView.java
@@ -1,19 +1,39 @@
 package com.vitalii.view;
 
+import com.vitalii.control.RaceManager;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class RaceView extends Application {
 
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setTitle("HBox Experiment 1");
+        primaryStage.setTitle("CarsRace!");
 
-        Button button = new Button("My Button");
+        Button button = new Button("Start the Race!");
 
-        Scene scene = new Scene(button, 200, 100);
+        Group root = new Group();
+
+        final RaceManager raceManager = new RaceManager(root);
+
+        Scene scene = new Scene(root,800, 600, Color.LIGHTSKYBLUE);
+
+        button.setLayoutX(350);
+        button.setLayoutY(550);
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                raceManager.startRace();
+            }
+        });
+
+        root.getChildren().add(button);
         primaryStage.setScene(scene);
         primaryStage.show();
 

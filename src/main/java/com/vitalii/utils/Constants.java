6314afc3937d5991521c4584c157d9c80a2763b9
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-07 20:20:31 +0300

    change coordinates of TUNNEL_ROAD_FINISH and ASPHALT2_ROAD_START, change DISTANCE_TO_RACE, add CARS_IN_TUNNEL constant

--- src/main/java/com/vitalii/utils/Constants.java
+++ src/main/java/com/vitalii/utils/Constants.java
@@ -2,7 +2,9 @@ package com.vitalii.utils;
 
 public class Constants {
     public static final int CARS_TO_RACE = 11;
-    public static final int DISTANCE_TO_RACE = 600;
+    public static final int DISTANCE_TO_RACE = 740;
+
+    public static final int CARS_IN_TUNNEL = 3;
 
     public static final int ASPHALT1_ROAD_START = 0;
     public static final int ASPHALT1_ROAD_FINISH = 149;
@@ -11,8 +13,8 @@ public class Constants {
     public static final int OFF_ROAD_FINISH = 299;
 
     public static final int TUNNEL_ROAD_START = 319;
-    public static final int TUNNEL_ROAD_FINISH = 399;
+    public static final int TUNNEL_ROAD_FINISH = 509;
 
-    public static final int ASPHALT2_ROAD_START = 419;
-    public static final int ASPHALT2_ROAD_FINISH = 590;
+    public static final int ASPHALT2_ROAD_START = 529;
+    public static final int ASPHALT2_ROAD_FINISH = 740;
 }

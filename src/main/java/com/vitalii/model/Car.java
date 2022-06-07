d4d7ffbe1b2bc04d31fe0fdf26c158756317f5d7
Vitalii Stinskii <vstinskij@gmail.com>
2022-06-07 19:55:14 +0300

    add road variable and method checkRoad to Car

--- src/main/java/com/vitalii/model/Car.java
+++ src/main/java/com/vitalii/model/Car.java
@@ -2,6 +2,8 @@ package com.vitalii.model;
 
 import javafx.application.Platform;
 
+import java.util.concurrent.Semaphore;
+
 public class Car {
 
     private String name;
@@ -10,8 +12,11 @@ public class Car {
     private int acceleration;
     private CarView carView;
     private int passedDistance = 0;
+    private Road road = Road.ASPHALT;
 
     private float step;
+
+    private Semaphore semaphore;
 
     public Car(String name, int speed, int maxSpeed, int acceleration, CarView carView) {
         this.name = name;
@@ -30,16 +35,36 @@ public class Car {
     public void move() {
         accelerate();
 
+        checkRoad();
+
+        if (road.equals(Road.OFF_ROAD)) {
+            speed = (int) (speed * 0.7);
+        }
+
         step = (float) speed / 70;
 
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    carView.getImageView().setX(carView.getImageView().getX() + step);
-                }
-            });
-            passedDistance = (int) carView.getImageView().getX();
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                carView.getImageView().setX(carView.getImageView().getX() + step);
+            }
+        });
+        passedDistance = (int) carView.getImageView().getX() + 75;
 
+    }
+
+    private void checkRoad() {
+        if (passedDistance >= 169 && passedDistance <= 299 && !road.equals(Road.OFF_ROAD)) {
+            road = Road.OFF_ROAD;
+        }
+
+        if (passedDistance >= 319 && passedDistance <= 399 && !road.equals(Road.TUNNEL)) {
+            road = Road.TUNNEL;
+        }
+
+        if (passedDistance >= 419 && passedDistance <= 590 && !road.equals(Road.ASPHALT)) {
+            road = Road.ASPHALT;
+        }
     }
 
     public String getName() {
@@ -76,5 +101,13 @@ public class Car {
 
     public void setPassedDistance(int passedDistance) {
         this.passedDistance = passedDistance;
+    }
+
+    public void setSemaphore(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+    public Road getRoad() {
+        return road;
     }
 }
